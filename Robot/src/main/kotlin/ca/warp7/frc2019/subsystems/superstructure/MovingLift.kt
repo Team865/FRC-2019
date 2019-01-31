f898994c33757eb3ef877e1b307ecabcea668816
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-01-31 11:49:37 -0500

    make superstructure call lift to make it move to a position

--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/superstructure/MovingLift.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/superstructure/MovingLift.kt
@@ -1,14 +1,21 @@
 package ca.warp7.frc2019.subsystems.superstructure
 
 import ca.warp7.actionkt.Action
+import ca.warp7.frc2019.subsystems.Lift
+import ca.warp7.frc2019.subsystems.LiftState
 import ca.warp7.frc2019.subsystems.Superstructure
 import ca.warp7.frc2019.subsystems.SuperstructureState
 
 object MovingLift : Action {
     val wantedPosition = WantedPosition()
+    var setpoint = 0.0
 
     override fun start() {
-        val setpoint = wantedPosition.toWantedLiftHeight()
+        setpoint = wantedPosition.toWantedLiftHeight()
+    }
+
+    override fun update() {
+        Lift.set(LiftState.kGoToPosition){ positionInput = setpoint}
     }
 
     override fun stop() {
