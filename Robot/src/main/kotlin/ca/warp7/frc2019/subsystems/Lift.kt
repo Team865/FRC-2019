21d67ec8a3e505b338f9b9f7209cc2b93302f501
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-01-30 22:33:55 -0500

    add GoToPosition

--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/Lift.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/Lift.kt
@@ -2,6 +2,7 @@ package ca.warp7.frc2019.subsystems
 
 import ca.warp7.frc.Subsystem
 import ca.warp7.frc2019.constants.LiftConstants
+import ca.warp7.frc2019.constants.LiftConstants.kMaxVelocityInchesPerSecond
 import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix.motorcontrol.can.VictorSPX
@@ -36,7 +37,8 @@ object Lift : Subsystem() {
                 master.set(ControlMode.Position, demandedPosition)
             }
             OutputType.Position ->{
-                //master.set(ControlMode.MotionMagic, )
+                //TODO learn how to use Motion Magic
+                //master.set(ControlMode.MotionMagic, demandedPosition, kMaxAcceleration, kMaxVelocityInchesPerSecond)
             }
             OutputType.Velocity ->{
                 TODO()
--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/LiftState.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/LiftState.kt
@@ -2,10 +2,11 @@ package ca.warp7.frc2019.subsystems
 
 import ca.warp7.actionkt.periodic
 import ca.warp7.actionkt.runOnce
+import ca.warp7.frc2019.subsystems.lift.GoToPosition
 import ca.warp7.frc2019.subsystems.lift.OpenLoopLift
 
 object LiftState {
     val kIdle = runOnce { }
     val kOpenLoop = OpenLoopLift
-    //val kGoToPosition =
+    val kGoToPosition = GoToPosition
 }
