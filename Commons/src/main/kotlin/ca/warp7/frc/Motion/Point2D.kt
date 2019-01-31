b4df35fc6d0b867b056b3cbcb45f27557645b9ce
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-01-31 10:45:17 -0500

    add 2d points

--- Commons/src/main/kotlin/ca/warp7/frc/Motion/Point2D.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/Motion/Point2D.kt
@@ -1,17 +1,23 @@
 package ca.warp7.frc.Motion
 
-object Point2D {
-    var  kX = 0.0
-    var kY = 0.0
-    var kHypotenuse = 0.0
+import koma.extensions.map
+import koma.mat
+import koma.matrix.Matrix
+import koma.pow
+import koma.sqrt
 
-    fun setXY(x: Double, y: Double){
-        kX = x
-        kY = y
-        kHypotenuse = Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0))
+
+val Matrix<Double>.hypotenuse get() = sqrt( map{ it.pow(2.0)}.elementSum())
+fun things(): Unit{
+    val points = arrayOf(
+            mat[0,1],
+            mat[1,2],
+            mat[2,3]
+    )
+
+    val distances = mutableListOf<Double>()
+    for (i in 1 until points.size){
+        distances.add((points[i] - points[i - 1]).hypotenuse)
     }
 
-    fun getX(): Double { return kX    }
-    fun getY(): Double { return kY    }
-    fun getHypotenuse(): Double { return kHypotenuse   }
 }
