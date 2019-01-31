1054061c6545f9354d62398df12afe43d82e63c9
EvanBialo <Evan62831@gmail.com>
2019-01-31 13:57:55 -0500

    remove t

--- Robot/src/main/kotlin/ca/warp7/frc2019/constants/OuttakeConstants.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/constants/OuttakeConstants.kt
@@ -1,6 +1,6 @@
 package ca.warp7.frc2019.constants
 
-object OutttakeConstants {
+object OuttakeConstants {
     const val kLeft = ElectricalConstants.kOuttakeLeftVictorId
     const val kRight = ElectricalConstants.kOuttakeRightVictorId
 }
