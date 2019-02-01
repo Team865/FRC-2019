a9214508a1c368a65d47b077b1633ae8953d5594
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-02-01 09:44:36 -0500

    switch to superstructure automatically

--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/LiftState.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/LiftState.kt
@@ -3,10 +3,12 @@ package ca.warp7.frc2019.subsystems
 import ca.warp7.actionkt.periodic
 import ca.warp7.actionkt.runOnce
 import ca.warp7.frc2019.subsystems.lift.GoToPosition
+import ca.warp7.frc2019.subsystems.lift.HoldPosition
 import ca.warp7.frc2019.subsystems.lift.OpenLoopLift
 
 object LiftState {
     val kIdle = runOnce { }
     val kOpenLoop = OpenLoopLift
     val kGoToPosition = GoToPosition
+    val kHoldPosition = HoldPosition
 }
--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/lift/GoToPosition.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/lift/GoToPosition.kt
@@ -3,6 +3,7 @@ package ca.warp7.frc2019.subsystems.lift
 import ca.warp7.actionkt.Action
 import ca.warp7.frc2019.constants.LiftConstants.kHomeHeightInches
 import ca.warp7.frc2019.subsystems.Lift
+import ca.warp7.frc2019.subsystems.LiftState
 
 
 object GoToPosition: Action {
@@ -19,7 +20,7 @@ object GoToPosition: Action {
     }
 
     override fun stop() {
-        Lift.set(HoldPosition)
+        Lift.set(LiftState.kHoldPosition)
     }
 
 }
