3503c2f008bfae793281c01313f9301e5e2bad49
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-03 12:08:23 -0500

    add hall effect constant

--- Robot/src/main/kotlin/ca/warp7/frc2019/constants/IntakeConstants.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/constants/IntakeConstants.kt
@@ -2,5 +2,5 @@ package ca.warp7.frc2019.constants
 
 object IntakeConstants {
     const val kVictor = ElectricalConstants.kIntakeVictorId
-    const val kSolenoid = ElectricalConstants.kIntakeSolenoidId
+    const val kSolenoid = ElectricalConstants.kIntakeSolenoidPort
 }
