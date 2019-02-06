25258180500208a64aef21db01f5f459a5fb9025
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-02-06 17:10:52 -0500

    Added intake and hatch outtake tests @sydneydacks

---
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/TestIntake.kt
@@ -0,0 +1,26 @@
+package ca.warp7.frc2019.test
+
+import ca.warp7.frc2019.constants.IntakeConstants
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.can.VictorSPX
+import edu.wpi.first.wpilibj.GenericHID
+import edu.wpi.first.wpilibj.Solenoid
+import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.wpilibj.XboxController
+
+class TestIntake : TimedRobot() {
+    lateinit var victor : VictorSPX
+    lateinit var controller : XboxController
+    lateinit var solenoid : Solenoid
+
+    override fun robotInit(){
+        victor = VictorSPX(IntakeConstants.kVictor)
+        controller = XboxController(0)
+        solenoid = Solenoid(IntakeConstants.kSolenoid)
+    }
+
+    override fun teleopPeriodic() {
+        victor.set(ControlMode.PercentOutput,controller.getTriggerAxis(GenericHID.Hand.kRight))
+        solenoid.set(controller.bButton)
+    }
+}
