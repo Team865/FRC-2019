45938956bf77b26a5a806a97a35ac5d4443c040a
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-11 18:35:20 -0500

    add NameableAction and write ActionQueueImpl

--- ActionKt/src/main/kotlin/ca/warp7/actionkt/ActionQueueImpl.kt
+++ ActionKt/src/main/kotlin/ca/warp7/actionkt/ActionQueueImpl.kt
@@ -1,6 +1,39 @@
 package ca.warp7.actionkt
 
 class ActionQueueImpl : ActionDSLImpl(), ActionQueue {
+
+    private val queue: MutableList<NameableAction> = mutableListOf()
+    var currentAction: NameableAction? = null
+    var started = false
+
     override operator fun Action.unaryPlus() {
+        if (!started) queue.add(NameableAction(this))
+    }
+
+    override fun start() {
+        super.start()
+        started = true
+    }
+
+    override val shouldFinish: Boolean
+        get() = super.shouldFinish || queue.isEmpty()
+
+    override fun update() {
+        super.update()
+        if (currentAction == null) {
+            if (queue.isEmpty()) return
+            currentAction = queue.removeAt(0)
+            currentAction?.action?.start()
+        }
+        currentAction?.action?.update()
+        if (currentAction?.action?.shouldFinish == true) {
+            currentAction?.action?.stop()
+            currentAction = null
+        }
+    }
+
+    override fun stop() {
+        currentAction?.action?.stop()
+        super.stop()
     }
 }
---
+++ ActionKt/src/main/kotlin/ca/warp7/actionkt/NameableAction.kt
@@ -0,0 +1,3 @@
+package ca.warp7.actionkt
+
+data class NameableAction(val action: Action, val name: String? = null)
