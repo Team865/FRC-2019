c5fbbf72538d096ae34373e4bb6bb6735260577b
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-02-13 21:55:11 -0500

    add tests

--- Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
@@ -1,11 +1,14 @@
 package ca.warp7.frc2019.test
 
+import ca.warp7.frc2019.test.Passthrough.Passthrough
 import ca.warp7.frc2019.test.conveyor_simple.ConveyorSimple
+import ca.warp7.frc2019.test.intake_simple.IntakeSimple
+import ca.warp7.frc2019.test.low_goal_bot.Lowbot
 import edu.wpi.first.wpilibj.TimedRobot
 
 object TestRobot {
     @JvmStatic
     fun main(args: Array<String>) {
-        TimedRobot.startRobot { ConveyorSimple() }
+        TimedRobot.startRobot { Lowbot() }
     }
 }
