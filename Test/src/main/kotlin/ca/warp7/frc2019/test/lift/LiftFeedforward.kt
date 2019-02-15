d5d72a66c9ab7c9413fe3df84610255633730244
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-15 10:07:40 -0500

    fix LiftFeedforward

--- Test/src/main/kotlin/ca/warp7/frc2019/test/lift/LiftFeedforward.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/lift/LiftFeedforward.kt
@@ -5,25 +5,38 @@ import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.DemandType
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix.motorcontrol.can.VictorSPX
+import edu.wpi.first.networktables.NetworkTableEntry
 import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import kotlin.math.absoluteValue
 import kotlin.math.withSign
 
 @Suppress("unused")
 class LiftFeedforward : TimedRobot() {
 
-    lateinit var liftMaster: TalonSRX
-    lateinit var xboxController: XboxController
+    private lateinit var liftMaster: TalonSRX
+    private lateinit var xboxController: XboxController
 
-    var feedforward = 0.05
-    var olr = 0.0
+    private var feedforward = 0.05
+    private var olr = 0.5
+
+    private lateinit var olrEntry: NetworkTableEntry
+    private lateinit var ffEntry: NetworkTableEntry
+
+    private var leftPressed = false
+    private var rightPressed = false
 
     override fun robotInit() {
         xboxController = XboxController(0)
         liftMaster = TalonSRX(LiftConstants.kMaster)
         VictorSPX(LiftConstants.kFollower).apply { inverted = true }.follow(liftMaster)
+        val tab = Shuffleboard.getTab("Lift Feedforward")
+        olrEntry = tab.add("ramp", 0.0)
+                .withPosition(0, 0).withSize(10, 3).entry
+        ffEntry = tab.add("feedforward", 0.0)
+                .withPosition(0, 4).withSize(10, 3).entry
     }
 
     override fun disabledInit() {
@@ -31,8 +44,15 @@ class LiftFeedforward : TimedRobot() {
     }
 
     override fun teleopPeriodic() {
-        if (xboxController.getBumper(GenericHID.Hand.kLeft)) feedforward -= 0.01
-        if (xboxController.getBumper(GenericHID.Hand.kRight)) feedforward += 0.01
+        if (xboxController.getBumper(GenericHID.Hand.kLeft) && !leftPressed) {
+            feedforward -= 0.01
+            leftPressed = true
+        } else leftPressed = false
+        if (xboxController.getBumper(GenericHID.Hand.kRight)) {
+            feedforward += 0.01
+            rightPressed = true
+        } else rightPressed = false
+        if (feedforward < 0.05) feedforward = 0.05
         var newOLR = olr
         if (xboxController.aButton) newOLR += 0.1
         if (xboxController.bButton) newOLR -= 0.1
@@ -41,10 +61,20 @@ class LiftFeedforward : TimedRobot() {
             liftMaster.configOpenloopRamp(newOLR, 0)
             olr = newOLR
         }
+        if (xboxController.yButton) {
+            feedforward = 0.05
+            olr = 0.0
+            liftMaster.configOpenloopRamp(olr, 0)
+        }
         val y = xboxController.getY(GenericHID.Hand.kLeft)
         liftMaster.set(ControlMode.PercentOutput,
                 if (y.absoluteValue > 0.2) -(y - 0.2.withSign(y)) / 0.8 * 0.5 else 0.0,
                 DemandType.ArbitraryFeedForward,
                 feedforward)
+    }
+
+    override fun robotPeriodic() {
+        olrEntry.setDouble(olr)
+        ffEntry.setDouble(feedforward)
     }
 }
