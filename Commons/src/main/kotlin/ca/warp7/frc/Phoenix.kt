851414bc018b31f1f1d925b6581d10436ea3d994
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-15 17:00:35 -0500

    add SpeedControlKt

--- Commons/src/main/kotlin/ca/warp7/frc/Phoenix.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/Phoenix.kt
@@ -5,10 +5,6 @@ package ca.warp7.frc
 import com.ctre.phoenix.motorcontrol.*
 import com.ctre.phoenix.motorcontrol.can.*
 import edu.wpi.first.wpilibj.SpeedController
-import edu.wpi.first.wpilibj.TimedRobot
-import kotlin.math.absoluteValue
-import kotlin.math.min
-import kotlin.math.withSign
 
 fun <T : TalonSRX> T.config(config: TalonSRXConfiguration) = apply { configAllSettings(config) }
 fun <T : VictorSPX> T.config(config: VictorSPXConfiguration) = apply { configAllSettings(config) }
@@ -33,34 +29,6 @@ private class UnsafeSpeedController(val base: BaseMotorController) : SpeedContro
 }
 
 fun <T : BaseMotorController> T.wpi(): SpeedController = UnsafeSpeedController(this)
-
-private class LinearRampedSpeedController(ramp: Double, val setter: (Double) -> Unit) : SpeedController {
-
-    var lastSpeed = 0.0
-    var sign = 1.0
-    val rate = TimedRobot.kDefaultPeriod / ramp
-
-    override fun getInverted(): Boolean = sign > 0
-    override fun pidWrite(output: Double) = set(output)
-    override fun stopMotor() = disable()
-    override fun get(): Double = lastSpeed * sign
-
-    override fun set(speed: Double) {
-        val diff = speed - lastSpeed
-        lastSpeed += min(diff.absoluteValue, rate).withSign(diff)
-        setter(lastSpeed * sign)
-    }
-
-    override fun setInverted(isInverted: Boolean) {
-        sign = if (isInverted) -1.0 else 1.0
-    }
-
-    override fun disable() {
-        lastSpeed = 0.0
-    }
-}
-
-fun linearRamp(ramp: Double, setter: (Double) -> Unit): SpeedController = LinearRampedSpeedController(ramp, setter)
 
 val kDefaultVictorSPX
     get() = VictorSPXConfiguration().apply {
