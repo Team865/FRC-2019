dd67448f2da86ebc38ce588504eefd9d7437e09e
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-02-16 18:32:43 -0500

    tune some values

--- Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
@@ -1,5 +1,8 @@
 package ca.warp7.frc2019.test
 
+import ca.warp7.frc2019.test.drive.DriveCurvature
+import ca.warp7.frc2019.test.lift.LiftFeedforward
+import ca.warp7.frc2019.test.low_goal_bot.Lowbot
 import ca.warp7.frc2019.test.commons.DriveWithCommons
 import edu.wpi.first.wpilibj.TimedRobot
 
--- Test/src/main/kotlin/ca/warp7/frc2019/test/low_goal_bot/Lowbot.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/low_goal_bot/Lowbot.kt
@@ -73,10 +73,9 @@ class Lowbot : TimedRobot() {
         } else if (right > 0.1) {
             speed = right * -1
         }
-        speed *= 0.7
 
-        rightConveyor.set(ControlMode.PercentOutput, speed / 2)
-        leftConveyor.set(ControlMode.PercentOutput, speed / 2)
+        rightConveyor.set(ControlMode.PercentOutput, speed * 0.7 / 2 )
+        leftConveyor.set(ControlMode.PercentOutput, speed * 0.7 / 2)
         intake.set(ControlMode.PercentOutput, speed)
 
         xSpeed += (controller.getY(GenericHID.Hand.kLeft) - xSpeed) / 6
@@ -89,8 +88,8 @@ class Lowbot : TimedRobot() {
         )
         val lt = controller.getTriggerAxis(GenericHID.Hand.kLeft)
         val rt = controller.getTriggerAxis(GenericHID.Hand.kRight)
-        val outspeed = if (lt > rt) lt else -rt
-        leftOuttake.set(ControlMode.PercentOutput, outspeed)
+        val outspeed = (if (lt > rt) lt else -rt) * 0.45 * 0.7
+        leftOuttake.set(ControlMode.PercentOutput, outspeed * -1)
         rightOuttake.set(ControlMode.PercentOutput, outspeed)
     }
 }
