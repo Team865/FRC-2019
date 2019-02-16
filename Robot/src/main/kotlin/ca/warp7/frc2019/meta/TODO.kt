68d121bd88ccd66cd4fc56c18f9e7ed1e3b03fb8
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-15 20:32:55 -0500

    add todo file

---
+++ Robot/src/main/kotlin/ca/warp7/frc2019/meta/TODO.kt
@@ -0,0 +1,21 @@
+package ca.warp7.frc2019.meta
+
+
+/*
+TODO("Template")
+    - TODO
+ */
+
+/*
+TODO("Template")
+    - Release code
+    - Superstructure states
+    - Find setpoint heights
+    - Adjust manual lift constants
+    - Add a state using lift motion planner
+    - Hatch outtake (panel mechanism)
+    - Implement async action
+    - Indexing cargo state tuning
+    - Add lazy actuators
+
+ */
