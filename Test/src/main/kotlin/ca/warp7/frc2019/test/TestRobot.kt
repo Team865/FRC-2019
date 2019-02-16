f6b56eafc8185c1540825db5d783406574167673
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-16 10:52:34 -0500

    add outtake tuning test

--- Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/TestRobot.kt
@@ -1,11 +1,11 @@
 package ca.warp7.frc2019.test
 
-import ca.warp7.frc2019.test.lift.feedforward.LiftFeedforward2
+import ca.warp7.frc2019.test.lift_outtake.tuning.LiftOuttakeTuning
 import edu.wpi.first.wpilibj.TimedRobot
 
 object TestRobot {
     @JvmStatic
     fun main(args: Array<String>) {
-        TimedRobot.startRobot { LiftFeedforward2() }
+        TimedRobot.startRobot { LiftOuttakeTuning() }
     }
 }
---
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/lift_outtake/tuning/LiftOuttakeTuning.kt
@@ -0,0 +1,58 @@
+package ca.warp7.frc2019.test.lift_outtake.tuning
+
+import ca.warp7.frc.*
+import ca.warp7.frc2019.subsystems.Conveyor
+import ca.warp7.frc2019.subsystems.Outtake
+import edu.wpi.first.wpilibj.TimedRobot
+
+@Suppress("unused")
+class LiftOuttakeTuning : TimedRobot() {
+
+    /**
+     * Initializes the robot by setting the state of subsystems
+     * directly or transitively and activating the controllers.
+     * Comment out the controller lines to disable them
+     */
+    override fun robotInit() {
+        println("Hello me is robit!")
+        setControllerMode(ControllerMode.DriverOnly)
+        LiftSubsystem.set { }
+        Outtake.set { }
+        Conveyor.set { }
+    }
+
+    /**
+     * Runs a periodic loop that collects inputs, update the autonomous
+     * routine and controller loop, process subsystem states, send output
+     * signals, and send telemetry data
+     */
+    override fun robotPeriodic() = runPeriodicLoop()
+
+    /**
+     * Disables the robot by disabling each subsystem and not calling
+     * output methods. Stops the autonomous routine
+     */
+    override fun disabledInit() = disableRobot()
+
+    /*
+    =====================================================
+    Starts various modes of the robot using control loops
+    =====================================================
+     */
+
+    override fun autonomousInit() = Unit
+    override fun teleopInit() = LiftOuttakeLoop.start()
+    override fun testInit() = LiftOuttakeLoop.start()
+
+    /*
+    =====================================================
+    The following periodic functions are not used because
+    they are all handled by the `robotPeriodic` function
+    =====================================================
+     */
+
+    override fun disabledPeriodic() = Unit
+    override fun autonomousPeriodic() = Unit
+    override fun teleopPeriodic() = Unit
+    override fun testPeriodic() = Unit
+}
