827db82b58ab61bba4c5d71239bb30bee3a2622f
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-16 19:44:59 -0500

    add shuffleboard values to Superstructure

--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/Superstructure.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/Superstructure.kt
@@ -1,5 +1,17 @@
 package ca.warp7.frc2019.subsystems
 
 import ca.warp7.frc.Subsystem
+import ca.warp7.frc2019.subsystems.superstructure.PassThrough
 
-object Superstructure : Subsystem()
+object Superstructure : Subsystem() {
+    override fun onPostUpdate() {
+        put("Conveyor Speed", Conveyor.speed)
+        put("Outtake Speed", Outtake.speed)
+        put("Outtake Grabbing", Outtake.grabbing)
+        put("Outtake Pushing", Outtake.pushing)
+        put("Intake Speed", Intake.speed)
+        put("Intake Extended", Intake.extended)
+        put("Climber Climbing", Climber.climbing)
+        put("PassThrough Speed", PassThrough.speed)
+    }
+}
