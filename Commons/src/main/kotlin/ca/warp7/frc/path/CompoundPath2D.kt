2b36c71406989eca1be785d5795c832f8f966feb
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-23 11:49:33 -0500

    change controlConstant

--- Commons/src/main/kotlin/ca/warp7/frc/path/CompoundPath2D.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/path/CompoundPath2D.kt
@@ -1,46 +1,55 @@
 package ca.warp7.frc.path
 
 @Suppress("unused")
-class SplinePath2D(private val segments: List<Path2D>) : Path2D {
+class CompoundPath2D(private val segments: List<Path2D>) : Path2D {
     private val count = segments.size
+    private val seg = 1.0 / segments.size
     override fun px(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].px((t - seg * i) / seg)
     }
 
     override fun py(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].py((t - seg * i) / seg)
     }
 
     override fun vx(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].vx((t - seg * i) / seg)
     }
 
     override fun vy(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].vy((t - seg * i) / seg)
     }
 
     override fun ax(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].ax((t - seg * i) / seg)
     }
 
     override fun ay(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].ay((t - seg * i) / seg)
     }
 
     override fun jx(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].jx((t - seg * i) / seg)
     }
 
     override fun jy(t: Double): Double {
         t.checkBounds()
-        TODO("not implemented")
+        val i = (count * t).toInt()
+        return segments[i].jy((t - seg * i) / seg)
     }
 
     override val dx: Double
