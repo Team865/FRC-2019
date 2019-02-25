80ddb303bf594a76983a810f63fee2def4acb244
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-25 18:34:25 -0500

    delete state package

--- Commons/src/main/kotlin/ca/warp7/frc/path/Path2DState.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/path/Path2DState.kt
@@ -2,8 +2,8 @@ package ca.warp7.frc.path
 
 data class Path2DState(
         val t: Double,
-        val x: Double,
-        val y: Double,
+        val px: Double,
+        val py: Double,
         val vx: Double,
         val vy: Double,
         val ax: Double,
