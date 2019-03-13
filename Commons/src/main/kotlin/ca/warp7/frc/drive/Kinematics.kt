a8470c23f993d85f7b888046500f5e850b253e07
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-12 22:31:33 -0400

    add maxFromCurvature

--- Commons/src/main/kotlin/ca/warp7/frc/drive/Kinematics.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/drive/Kinematics.kt
@@ -2,6 +2,8 @@
 
 package ca.warp7.frc.drive
 
+import ca.warp7.frc.epsilonEquals
+
 fun DifferentialDriveModel.solve(state: WheelState) = ChassisState(
         linear = (state.left + state.right) / 2.0,
         angular = (state.right - state.left) / (2 * wheelbaseRadius)
@@ -11,3 +13,24 @@ fun DifferentialDriveModel.solve(state: ChassisState) = WheelState(
         left = state.linear - state.angular * wheelbaseRadius,
         right = state.linear + state.angular * wheelbaseRadius
 )
+
+/**
+ * Calculates the maximum reachable linear and angular velocity based on the curvature.
+ * The result is unsigned.
+ *
+ * The equations are derived from `w(r + L / 2) = far side velocity`. Assume far side goes 100%,
+ * we replace it with max velocity and isolate for angular velocity.
+ *
+ * Then we rearrange `w = (right - left)/L` into `left = maxV - wL`, substitute it into
+ * `v = (left + right) / 2`, and get the equation for max linear velocity
+ *
+ * L is double of wheelBaseRadius, so calculations are simplified here.
+ *
+ * If curvature is 0, we just return a ChassisState with no angular velocity
+ */
+fun DifferentialDriveModel.maxFromCurvature(curvature: Double): ChassisState {
+    if (curvature.epsilonEquals(0.0)) return ChassisState(linear = maxVelocity, angular = 0.0)
+    val angular = maxVelocity / (1 / curvature + wheelbaseRadius)
+    val linear = maxVelocity - angular * wheelbaseRadius
+    return ChassisState(linear = linear, angular = angular)
+}
