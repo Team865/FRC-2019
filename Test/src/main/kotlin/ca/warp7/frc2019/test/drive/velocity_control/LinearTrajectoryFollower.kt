0e88ea970dc321c8eaaf170cbdf47202c8d0e21e
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-16 17:47:14 -0400

    tune velocity pid

--- Test/src/main/kotlin/ca/warp7/frc2019/test/drive/velocity_control/LinearTrajectoryFollower.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/drive/velocity_control/LinearTrajectoryFollower.kt
@@ -1,6 +1,7 @@
 package ca.warp7.frc2019.test.drive.velocity_control
 
 import ca.warp7.actionkt.Action
+import ca.warp7.frc.geometry.interpolate
 import ca.warp7.frc.interpolate
 import ca.warp7.frc2019.constants.DriveConstants
 import ca.warp7.frc2019.subsystems.Drive
@@ -9,35 +10,57 @@ import edu.wpi.first.wpilibj.Timer
 
 @Suppress("MemberVisibilityCanBePrivate")
 class LinearTrajectoryFollower : Action {
-    val trajectory = LinearTrajectory(25.0)
+    val trajectory = LinearTrajectory(12.0)
     val moments = trajectory.moments
     val totalTime = moments.last().t
     var t = 0.0
     var i = 0
     var startTime = 0.0
+    var lastTime = 0.0
 
     override fun start() {
         startTime = Timer.getFPGATimestamp()
     }
 
     override fun update() {
-        t = Timer.getFPGATimestamp() - startTime
+        val nt = Timer.getFPGATimestamp()
+        val dt = nt - lastTime
+        lastTime = nt
+        t = nt - startTime
         while (i < moments.size - 3 && moments[i].t < t) i++
         val mi = moments[i]
         val mj = moments[i + 1]
         val n = (t - mi.t) / (mj.t - mi.t)
+        val p = mi.v.state.interpolate(mj.v.state, n).mag
         val v = interpolate(mi.v.velocity, mj.v.velocity, n)
         val a = interpolate(mi.v.acceleration, mj.v.acceleration, n)
         Drive.apply {
             controlMode = ControlMode.Velocity
-            leftDemand = (v * 0.0254 * DriveConstants.kTicksPerInch) / 10
-            rightDemand = (v * 0.0254 * DriveConstants.kTicksPerInch) / 10
-            println("$leftDemand, $rightDemand")
-            leftFeedforward = a / trajectory.maxAcceleration / 1023
-            rightFeedforward = a / trajectory.maxAcceleration / 1023
+            val leftPos = Drive.leftPosition / DriveConstants.kTicksPerInch * 0.0254
+            val rightPos = Drive.rightPosition / DriveConstants.kTicksPerInch * 0.0254
+            println("$leftPos, $rightPos, $p")
+            leftDemand = (v / 0.0254 * DriveConstants.kTicksPerInch) / 10 + (a / 0.0254 * DriveConstants.kTicksPerInch) / 23
+            rightDemand = (v / 0.0254 * DriveConstants.kTicksPerInch) / 10 + (a / 0.0254 * DriveConstants.kTicksPerInch) / 23
+            //leftFeedforward = a / trajectory.maxAcceleration
+            //rightFeedforward = a / trajectory.maxAcceleration
         }
     }
 
     override val shouldFinish: Boolean
         get() = t > totalTime || i >= moments.size
+
+    override fun stop() {
+        Drive.apply {
+            leftDemand = 0.0
+            rightDemand = 0.0
+            leftFeedforward = 0.0
+            rightFeedforward = 0.0
+        }
+    }
 }
+
+fun main() {
+    LinearTrajectory(10.0).moments.forEach { println(it) }
+}
+
+// m/s^2 to ticks/(0.1s)^2
--- Test/src/main/kotlin/ca/warp7/frc2019/test/drive/velocity_control/VelocityControlLoop.kt
+++ Test/src/main/kotlin/ca/warp7/frc2019/test/drive/velocity_control/VelocityControlLoop.kt
@@ -1,41 +1,14 @@
 package ca.warp7.frc2019.test.drive.velocity_control
 
 import ca.warp7.actionkt.Action
-import ca.warp7.frc.set
 import ca.warp7.frc2019.subsystems.Drive
 import com.ctre.phoenix.motorcontrol.ControlMode
 
 object VelocityControlLoop : Action {
 
     override fun start() {
-        Drive.set {
-            leftMaster.configFactoryDefault(0)
-            rightMaster.configFactoryDefault(0)
-            leftMaster.enableVoltageCompensation(false)
-            rightMaster.enableVoltageCompensation(false)
-            leftMaster.config_kP(1, 0.5, 0)
-            leftMaster.config_kI(1, 0.0, 0)
-            leftMaster.config_kD(1, 5.0, 0)
-            leftMaster.config_kF(1, 1.0, 0)
-            leftMaster.configOpenloopRamp(0.0, 0)
-            rightMaster.config_kP(1, 0.5, 0)
-            rightMaster.config_kI(1, 0.0, 0)
-            rightMaster.config_kD(1, 5.0, 0)
-            rightMaster.config_kF(1, 1.0, 0)
-            rightMaster.configOpenloopRamp(0.0, 0)
-            leftMaster.selectProfileSlot(1, 0)
-            rightMaster.selectProfileSlot(1, 0)
-        }
         Drive.controlMode = ControlMode.Velocity
-        //Drive.set(LinearTrajectoryFollower())
-        //Drive.set(SimpleTrapezoid)
-    }
-
-    override fun update() {
-        Drive.leftDemand = -200.0
-        Drive.rightDemand = -300.0
-        Drive.leftFeedforward = 0.0
-        Drive.rightFeedforward = 0.0
+        Drive.set(LinearTrajectoryFollower())
     }
 
     override val shouldFinish: Boolean
