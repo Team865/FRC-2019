48d2e573e17a22a1664d93e66efd0a4ecf07bec7
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-28 12:48:44 -0400

    update

--- PathPlanner/src/main/kotlin/ca/warp7/pathplanner/PathPlanner.kt
+++ PathPlanner/src/main/kotlin/ca/warp7/pathplanner/PathPlanner.kt
@@ -13,44 +13,44 @@ import kotlin.math.sqrt
 @Suppress("MemberVisibilityCanBePrivate")
 class PathPlanner : PApplet() {
 
+    data class ControlPoint(
+            var pos: Translation2D,
+            var heading: Translation2D,
+            var dir: Translation2D
+    )
+
     override fun settings() {
         size(768, 512)
     }
+
+    val kTriangleRatio = 1 / sqrt(3.0)
+    val step = 0.0254 * 12 / 10
+    val pAng = Rotation2D.fromDegrees(1.0)
+    val nAng = Rotation2D.fromDegrees(-1.0)
+    val reversedRotation = Rotation2D(-1.0, 0.0)
 
     val wheelBaseRadius = inchesToMeters(12.4)
     val maxVel = feetToMeters(12.0)
 
     val kPixelsPerMeter = 494 / 8.2296
-
-    val Double.my2x: Double
-        get() = (17.0 + (512.0 - 17.0) / 2 + kPixelsPerMeter * this)
-
-    val Double.mx2y: Double
-        get() = (493.0 - kPixelsPerMeter * this)
-
+    val Double.my2x: Double get() = (17.0 + (512.0 - 17.0) / 2 + kPixelsPerMeter * this)
+    val Double.mx2y: Double get() = (493.0 - kPixelsPerMeter * this)
     val Translation2D.newXY get() = Translation2D(y.my2x, x.mx2y)
+    val Translation2D.newXYNoOffset get() = Translation2D(kPixelsPerMeter * y, -kPixelsPerMeter * x)
 
     var waypoints: Array<Pose2D> = emptyArray()
-
     var intermediate: List<QuinticSegment2D> = emptyList()
-
     var splines: List<CurvatureState<Pose2D>> = listOf()
-
     var controlPoints = mutableListOf<ControlPoint>()
 
     var curvatureSum = 0.0
-
     var arcLength = 0.0
 
-    data class ControlPoint(var pos: Translation2D, var heading: Translation2D, var dir: Translation2D)
+    var selectedIndex = -1
+    var selectionChanged = false
 
-    fun lineTo(a: Translation2D, b: Translation2D) =
-            line(a.x.toFloat(), a.y.toFloat(), b.x.toFloat(), b.y.toFloat())
-
+    fun lineTo(a: Translation2D, b: Translation2D) = line(a.x.toFloat(), a.y.toFloat(), b.x.toFloat(), b.y.toFloat())
     fun vertex(a: Translation2D) = vertex(a.x.toFloat(), a.y.toFloat())
-
-    val kTriangleRatio = 1 / sqrt(3.0)
-
     val bg = PImage(ImageIO.read(PathPlanner::class.java.getResource("/field.PNG")))
 
     override fun setup() {
@@ -67,7 +67,7 @@ class PathPlanner : PApplet() {
         controlPoints.clear()
         waypoints.forEach {
             val pos = it.translation.newXY
-            val heading = (it.translation + it.rotation.translation.scaled(0.5))
+            val heading = (it.translation + it.rotation.translation.scaled(0.5)).newXY  //FIXME
             val dir = it.rotation.norm.translation
             controlPoints.add(ControlPoint(pos, heading, dir))
         }
@@ -86,10 +86,10 @@ class PathPlanner : PApplet() {
         val r3 = r1.rotate(Rotation2D(0.0, -1.0)).scaled(kTriangleRatio)
         ellipse(pos.x.toFloat(), pos.y.toFloat(), 12f, 12f)
         val start = pos + dir.scaled(6.0).run { Translation2D(y, -x) }
-        lineTo(start, heading.newXY)
-        val a1 = (heading + r1).newXY
-        val a2 = (heading + r2).newXY
-        val a3 = (heading + r3).newXY
+        lineTo(start, heading)
+        val a1 = heading + r1.newXYNoOffset
+        val a2 = heading + r2.newXYNoOffset
+        val a3 = heading + r3.newXYNoOffset
         beginShape()
         vertex(a1)
         vertex(a2)
@@ -117,6 +117,7 @@ class PathPlanner : PApplet() {
         strokeWeight(2f)
 
         val maxK = splines.maxBy { it.curvature.absoluteValue }?.curvature?.absoluteValue ?: 1.0
+        // draw the curve
         for (i in 1 until splines.size) {
             t = splines[i].state.translation
             normal = (splines[i].state.rotation.normal * wheelBaseRadius).translation
@@ -142,6 +143,7 @@ class PathPlanner : PApplet() {
                 "minRadius: ${metersToFeet(1 / maxK).f}ft\n" +
                 "vel@min ${metersToFeet(linear).f}ft/s\n" +
                 "arcLength: ${metersToFeet(arcLength).f}ft\n"
+        // draw control points
         controlPoints.forEachIndexed { index, controlPoint ->
             if (selectedIndex == index) {
                 fill(255f, 255f, 255f)
@@ -185,16 +187,13 @@ class PathPlanner : PApplet() {
         }
     }
 
-    var selectedIndex = -1
-    var selectionChanged = false
-
     override fun mouseClicked() {
         var found = false
         controlPoints.forEachIndexed { index, controlPoint ->
             val mouse = Translation2D(mouseX.toDouble(), mouseY.toDouble())
             val pc = (controlPoint.pos - mouse).mag
-            val hc = (controlPoint.heading.newXY - mouse).mag
-            if (pc < 16 || hc < 16) {
+            val hc = (controlPoint.heading - mouse).mag
+            if (pc < 12 || hc < 12) {
                 selectionChanged = true
                 found = true
                 selectedIndex = (if (selectedIndex != index) index else -1)
@@ -206,10 +205,26 @@ class PathPlanner : PApplet() {
         }
     }
 
-    val step = 0.0254 * 12 / 10
-    val pAngStep = Rotation2D.fromDegrees(1.0)
-    val nAngStep = Rotation2D.fromDegrees(-1.0)
-    val reversedRotation = Rotation2D(-1.0, 0.0)
+    var dragging = false
+
+    override fun mouseDragged() {
+        if (selectedIndex == -1) return
+        val mouse = Translation2D(mouseX.toDouble(), mouseY.toDouble())
+        val controlPoint = controlPoints[selectedIndex]
+        val waypoint = waypoints[selectedIndex]
+        val pc = controlPoint.pos - mouse
+        if (pc.mag < 16 || dragging) {
+            dragging = true
+            redrawScreen()
+            val heading = (mouse + waypoint.rotation.translation.scaled(0.5)).newXY
+            val dir = waypoint.rotation.norm.translation
+            ControlPoint(mouse, heading, dir).drawArrow()
+        }
+    }
+
+    override fun mouseReleased() {
+        dragging = false
+    }
 
     override fun keyPressed() {
         if (selectedIndex == -1) processDeselected()
@@ -219,38 +234,10 @@ class PathPlanner : PApplet() {
     fun processDeselected() {
         when {
             key.toInt() == CODED -> when (keyCode) {
-                PConstants.UP -> {
-                    for (i in 0 until waypoints.size) {
-                        waypoints[i] = waypoints[i].run {
-                            Pose2D(translation.transform(Translation2D(step, 0.0)), rotation)
-                        }
-                    }
-                    regenerateSplines()
-                }
-                PConstants.DOWN -> {
-                    for (i in 0 until waypoints.size) {
-                        waypoints[i] = waypoints[i].run {
-                            Pose2D(translation.transform(Translation2D(-step, 0.0)), rotation)
-                        }
-                    }
-                    regenerateSplines()
-                }
-                PConstants.LEFT -> {
-                    for (i in 0 until waypoints.size) {
-                        waypoints[i] = waypoints[i].run {
-                            Pose2D(translation.transform(Translation2D(0.0, -step)), rotation)
-                        }
-                    }
-                    regenerateSplines()
-                }
-                PConstants.RIGHT -> {
-                    for (i in 0 until waypoints.size) {
-                        waypoints[i] = waypoints[i].run {
-                            Pose2D(translation.transform(Translation2D(0.0, step)), rotation)
-                        }
-                    }
-                    regenerateSplines()
-                }
+                PConstants.UP -> translateAll(Translation2D(step, 0.0))
+                PConstants.DOWN -> translateAll(Translation2D(-step, 0.0))
+                PConstants.LEFT -> translateAll(Translation2D(0.0, -step))
+                PConstants.RIGHT -> translateAll(Translation2D(0.0, step))
             }
             key == 'r' -> {
                 val newWaypoints = waypoints.reversedArray()
@@ -273,46 +260,32 @@ class PathPlanner : PApplet() {
         }
     }
 
+    fun translateSelected(by: Translation2D) {
+        waypoints[selectedIndex] = waypoints[selectedIndex].run { Pose2D(translation + by, rotation) }
+        regenerateSplines()
+    }
+
+    fun rotateSelected(by: Rotation2D) {
+        waypoints[selectedIndex] = waypoints[selectedIndex].run { Pose2D(translation, rotation + by) }
+        regenerateSplines()
+    }
+
+    fun translateAll(by: Translation2D) {
+        for (i in 0 until waypoints.size) waypoints[i] = waypoints[i]
+                .run { Pose2D(translation + by, rotation) }
+        regenerateSplines()
+    }
+
     fun processSelected() {
         when {
             key.toInt() == CODED -> when (keyCode) {
-                PConstants.UP -> {
-                    waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                        Pose2D(translation.transform(Translation2D(step, 0.0)), rotation)
-                    }
-                    regenerateSplines()
-                }
-                PConstants.DOWN -> {
-                    waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                        Pose2D(translation.transform(Translation2D(-step, 0.0)), rotation)
-                    }
-                    regenerateSplines()
-                }
-                PConstants.LEFT -> {
-                    waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                        Pose2D(translation.transform(Translation2D(0.0, -step)), rotation)
-                    }
-                    regenerateSplines()
-                }
-                PConstants.RIGHT -> {
-                    waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                        Pose2D(translation.transform(Translation2D(0.0, step)), rotation)
-                    }
-                    regenerateSplines()
-                }
+                PConstants.UP -> translateSelected(Translation2D(step, 0.0))
+                PConstants.DOWN -> translateSelected(Translation2D(-step, 0.0))
+                PConstants.LEFT -> translateSelected(Translation2D(0.0, -step))
+                PConstants.RIGHT -> translateSelected(Translation2D(0.0, step))
             }
-            key == 'q' -> {
-                waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                    Pose2D(translation, rotation.rotate(nAngStep))
-                }
-                regenerateSplines()
-            }
-            key == 'w' -> {
-                waypoints[selectedIndex] = waypoints[selectedIndex].run {
-                    Pose2D(translation, rotation.rotate(pAngStep))
-                }
-                regenerateSplines()
-            }
+            key == 'q' -> rotateSelected(nAng)
+            key == 'w' -> rotateSelected(pAng)
             key == 'n' -> {
                 val newWaypoints = arrayOfNulls<Pose2D>(waypoints.size + 1)
                 for (i in 0..selectedIndex) newWaypoints[i] = waypoints[i]
