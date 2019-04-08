1a519ef15776fc599ae3d57d4675ab466e87a657
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-04-08 18:34:25 -0400

    remove Kinematics.kt

--- Commons/src/main/kotlin/ca/warp7/frc/drive/DifferentialDriveModel.kt
+++ Commons/src/main/kotlin/ca/warp7/frc/drive/DifferentialDriveModel.kt
@@ -1,38 +1,86 @@
 package ca.warp7.frc.drive
 
+import ca.warp7.frc.epsilonEquals
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.withSign
 
+/**
+ * Model of the kinematics and dynamics of a differential drive
+ */
 @Suppress("unused")
 data class DifferentialDriveModel(
-        val wheelRadius: Double,
-        val wheelbaseRadius: Double,
-        val maxVelocity: Double,
-        val maxAcceleration: Double,
-        val maxFreeSpeed: Double,
-        val speedPerVolt: Double,
-        val torquePerVolt: Double,
-        val frictionVoltage: Double,
-        val linearInertia: Double, // mass in kg
-        val angularInertia: Double,
-        val maxVolts: Double,
-        val angularDrag: Double = 1.0
+        val wheelRadius: Double, // m
+        val wheelbaseRadius: Double, // m (effective wheelbase)
+        val maxVelocity: Double, // m/s
+        val maxAcceleration: Double, // m/s^2
+        val maxFreeSpeed: Double, // m/s
+        val speedPerVolt: Double, // m/s per V (free speed of transmission per volt)
+        val torquePerVolt: Double, // N m per V (stall torque of transmission per volt)
+        val frictionVoltage: Double, // V (voltage that breaks static friction)
+        val linearInertia: Double, // kg (equivalent mass)
+        val angularInertia: Double, // kg*m^2 (equivalent moment of inertia)
+        val maxVolts: Double, // V (maximum voltage of transmission)
+        val angularDrag: Double = 1.0 // (N*m)/(m/s)
 ) {
+
+
+    /**
+     * Solves the forward kinematics of the drive train by converting the
+     * speeds/accelerations of wheels on each side into linear and angular
+     * speed/acceleration
+     */
     fun solve(wheels: WheelState) = ChassisState(
             linear = (wheels.left + wheels.right) / 2.0,
             angular = (wheels.right - wheels.left) / (2 * wheelbaseRadius)
     )
 
+
+    /**
+     * Solves the inverse kinematics of the drive train by converting linear
+     * and angular speed/acceleration into the speeds/accelerations of wheels
+     * on each side
+     */
     fun solve(chassis: ChassisState) = WheelState(
             left = chassis.linear - chassis.angular * wheelbaseRadius,
             right = chassis.linear + chassis.angular * wheelbaseRadius
     )
 
-    fun signedMaxAtCurvature(curvature: Double, maxVel: Double = maxVelocity) =
-            signedMaxAtCurvature(curvature, maxVel, wheelbaseRadius)
 
+    /**
+     * Calculates the maximum reachable linear and angular velocity based on the curvature.
+     * The result is unsigned.
+     *
+     * The equations are derived from `w(r + L / 2) = far side velocity`. Assume far side goes 100%,
+     * we replace it with max velocity and isolate for angular velocity.
+     *
+     * Then we rearrange `w = (right - left)/L` into `left = maxV - wL`, substitute it into
+     * `v = (left + right) / 2`, and get the equation for max linear velocity
+     *
+     * L is double of wheelBaseRadius, so calculations are simplified here.
+     *
+     * If curvature is 0, we just return a ChassisState with no angular velocity.
+     *
+     * Future: Does this also work for acceleration???
+     */
+    fun signedMaxAtCurvature(curvature: Double, maxVel: Double = maxVelocity): ChassisState {
+        if (curvature.epsilonEquals(0.0, 1E-9)) {
+            return ChassisState(maxVel, angular = 0.0)
+        }
+        val angular = maxVel / (1 / abs(curvature) + wheelbaseRadius)
+        val linear = maxVel - (angular * wheelbaseRadius)
+        return ChassisState(linear, angular.withSign(curvature))
+    }
+
+    /**
+     * Solves the results from above into wheel velocities
+     */
     fun solvedMaxAtCurvature(curvature: Double) = solve(signedMaxAtCurvature(curvature))
 
+    /**
+     * Get the free speed obtained at a specified voltage
+     */
     fun freeSpeedAtVoltage(voltage: Double): Double {
         return when {
             voltage > kEpsilon -> max(0.0, voltage - frictionVoltage) * speedPerVolt
@@ -41,29 +89,42 @@ data class DifferentialDriveModel(
         }
     }
 
-    fun torqueForVoltage(outputSpeed: Double, voltage: Double): Double {
+    /**
+     * Get the motor torque for a specified speed and voltage
+     */
+    fun torqueForVoltage(speed: Double, voltage: Double): Double {
+        // calculate the effective voltage (taking away the friction voltage)
         var effectiveVoltage = voltage
         when {
-            outputSpeed > kEpsilon -> effectiveVoltage -= frictionVoltage
-            outputSpeed < -kEpsilon -> effectiveVoltage += frictionVoltage
+            speed > kEpsilon -> effectiveVoltage -= frictionVoltage
+            speed < -kEpsilon -> effectiveVoltage += frictionVoltage
             voltage > kEpsilon -> effectiveVoltage = max(0.0, voltage - frictionVoltage)
             voltage < -kEpsilon -> effectiveVoltage = min(0.0, voltage + frictionVoltage)
             else -> return 0.0
         }
-        return torquePerVolt * (effectiveVoltage - outputSpeed / speedPerVolt)
+        // calculate torque based on the voltage left
+        return torquePerVolt * (effectiveVoltage - speed / speedPerVolt)
     }
 
-    fun voltageForTorque(output_speed: Double, torque: Double): Double {
+    /**
+     * Get the motor voltage for a specified speed and torque
+     */
+    fun voltageForTorque(speed: Double, torque: Double): Double {
+        // find out the sign of the friction voltage that needs to be applied
         val frictionVoltage = when {
-            output_speed > kEpsilon -> this.frictionVoltage
-            output_speed < -kEpsilon -> -this.frictionVoltage
+            speed > kEpsilon -> this.frictionVoltage
+            speed < -kEpsilon -> -this.frictionVoltage
             torque > kEpsilon -> this.frictionVoltage
             torque < -kEpsilon -> -this.frictionVoltage
             else -> return 0.0
         }
-        return torque / torquePerVolt + output_speed / speedPerVolt + frictionVoltage
+        // convert to volts and add the signed friction voltage
+        return torque / torquePerVolt + speed / speedPerVolt + frictionVoltage
     }
 
+    /**
+     * Solves the kinematic state of the robot into the dynamic state
+     */
     fun solve(kinematicState: KinematicState): DynamicState {
 
         val velocity = kinematicState.velocity
@@ -87,6 +148,6 @@ data class DifferentialDriveModel(
     }
 
     companion object {
-        const val kEpsilon = 1E-12
+        const val kEpsilon = 1E-9
     }
 }
