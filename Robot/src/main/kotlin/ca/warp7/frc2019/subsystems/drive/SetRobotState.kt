68a5d7ce4f31062422ec9af887abbbf7ffb1124a
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-04-08 14:50:02 -0400

    fix deprecated package name

--- Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/drive/SetRobotState.kt
+++ Robot/src/main/kotlin/ca/warp7/frc2019/subsystems/drive/SetRobotState.kt
@@ -1,10 +1,15 @@
 package ca.warp7.frc2019.subsystems.drive
 
 import ca.warp7.actionkt.Action
+import ca.warp7.frc.geometry.Pose2D
 import ca.warp7.frc.path.waypoint
 
-class SetRobotState(val xInFeet: Number, val yInFeet: Number, val angleInDegrees: Number) : Action {
+class SetRobotState(val state: Pose2D) : Action {
+
+    constructor(xInFeet: Number, yInFeet: Number, angleInDegrees: Number) :
+            this(waypoint(xInFeet, yInFeet, angleInDegrees))
+
     override fun start() {
-        DriveMotionPlanner.robotState = waypoint(xInFeet, yInFeet, angleInDegrees)
+        DriveMotionPlanner.robotState = state
     }
 }
